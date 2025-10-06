8e22d623844a2ec384c6dbddcc36be8cd942c935
exec432 <exec432@gmail.com>
2025-10-06 20:42:58 +0200

    Nearly perfect pils

--- template-parts/homepage.php
+++ template-parts/homepage.php
@@ -57,7 +57,7 @@ $pill_image_3 = get_field('pill_image_3', $post_id);
 ?>
 <section class="bg-secondary py-12 rounded-t-[60px] relative -mt-[10vh] z-20">
   <div class=" ml-0 xl:ml-28 w-screen">
-    <div class="ml-10 md:ml-0 w-full items-center flex md:hidden">
+    <div class="ml-10 w-full items-center flex min-[1101px]:hidden">
       <img src="<?php echo get_template_directory_uri(); ?>/assets/left_c.svg" alt="" class="mr-5 w-[10vw]">
       <h4 class="text-light text-[2rem] libre-baskerville-regular"><?php _e('Dlaczego my?', $text_domain); ?></h4>
     </div>
@@ -88,79 +88,72 @@ $pill_image_3 = get_field('pill_image_3', $post_id);
       }
       $doubleWidth = $imgWidth * 2;
       ?>
-      <div class="md:hidden mb-8 w-screen max-w-none">
-        <div class="scrollable-wrapper w-screen max-w-none">
-          <div class="scrollable-content w-screen max-w-none">
+      <div class="hidden [@media(width<=1100px)]:flex mb-8 w-screen justify-center items-center flex-col pl-8">
 
             <!-- PILL DEKORACYJNY -->
-            <div class="block w-screen max-w-none">
-              <div class="relative w-screen h-[40vh] sm:h-[45vh] rounded-full bg-cover bg-center"
-                style="background-image: url(<?= esc_url($pill_image) ?>);">
+            <div class="relative w-full md:w-[75%] h-[25vh] sm:h-[30vh] md:h-[40vh] xl:h-[35vh] object-cover object-center rounded-full mx-auto" 
+                style="background: url(<?= esc_url($pill_image) ?>); background-size: cover; background-position: center;">
                 <img src="<?= esc_url($leftPathURL) ?>" alt="C-left" class="absolute left-0 top-0 z-10 h-full">
-              </div>
             </div>
-            
+              
             <!-- PILL 1 -->
-            <div class="relative my-12 sm:my-16 w-screen h-[40vh] sm:h-[45vh] rounded-full flex items-center">
-              <img src="<?= esc_url($leftPathURL) ?>" alt="C-left" class="absolute left-0 top-0 z-10 h-full opacity-0">
-              <div class="text-white flex-1 px-[60px] sm:px-[80px]">
-                <h2 class="text-left tracking-[0.1em] text-[3vh] sm:text-[3.5vh] font-bold mb-2 libre-baskerville-regular uppercase">
+            <div class="relative flex justify-center items-center h-[25vh] sm:h-[30vh] md:h-[40vh] xl:h-[35vh]  my-12 w-full md:w-[75%] text-white px-[20%]
+              <img src="<?= esc_url($leftPathURL) ?>" alt="c-left" class="absolute top-0 left-0 h-full opacity-0">
+              <div class="flex flex-col justify-center w-full">
+                <h2 class="libre-baskerville-regular text-left text-[2.5vh] justify-evenly">
                   <?php _e('DOŚWIADCZENIE', $text_domain) ?>
                 </h2>
-                <p class="text-left text-[3vw] sm:text-[3.5vw] inter-regular">
+                <p class="inter-regular text-left justify-evenly text-[1.5vh]">
                   <?php _e('Lata praktyki w międzynarodowej administracji biznesowej', $text_domain) ?>
                 </p>
               </div>
-              <img src="<?= esc_url($rightPathURL) ?>" alt="C-right" class="absolute right-0 top-0 z-10 h-full">
+              <img src="<?= esc_url($rightPathURL) ?>" alt="c-right" class="absolute top-0 right-0 h-full">
             </div>
 
             <!-- PILL 2 -->
-            <div class="relative my-12 sm:my-16 w-screen h-[40vh] sm:h-[45vh] rounded-full flex items-center">
-              <img src="<?= esc_url($leftPathURL) ?>" alt="C-left" class="absolute left-0 top-0 z-10 h-full">
-              <div class="text-white flex-1 px-[60px] sm:px-[80px]">
-                <h2 class="text-left tracking-[0.1em] text-[3vh] sm:text-[3.5vh] font-bold mb-2 libre-baskerville-regular uppercase">
+            <div class="relative flex justify-center items-center h-[25vh] sm:h-[30vh] md:h-[40vh] xl:h-[35vh]  my-12 w-full md:w-[75%] text-white px-[20%]">
+              <img src="<?= esc_url($leftPathURL) ?>" alt="c-left" class="absolute top-0 left-0 h-full">
+              <div class="flex flex-col justify-center w-full">
+                <h2 class="libre-baskerville-regular text-left text-[2.5vh] justify-evenly">
                   <?php _e('INDYWIDUALNE PODEJŚCIE', $text_domain) ?>
                 </h2>
-                <p class="text-left text-[3vw] sm:text-[3.5vw] inter-regular">
+                <p class="inter-regular text-left justify-evenly text-[1.5vh]">
                   <?php _e('Strategia dopasowana do Twoich potrzeb', $text_domain) ?>
                 </p>
               </div>
-              <img src="<?= esc_url($rightPathURL) ?>" alt="C-right" class="absolute right-0 top-0 z-10 h-full opacity-0">
+              <img src="<?= esc_url($rightPathURL) ?>" alt="c-right" class="absolute top-0 right-0 h-full opacity-0">
             </div>
-
+            
             <!-- PILL 3 -->
-            <div class="relative my-12 sm:my-16 w-screen h-[40vh] sm:h-[45vh] rounded-full flex items-center">
-              <img src="<?= esc_url($leftPathURL) ?>" alt="C-left" class="absolute left-0 top-0 z-10 h-full opacity-0">
-              <div class="text-white flex-1 px-[60px] sm:px-[80px]">
-                <h2 class="text-left tracking-[0.1em] text-[3vh] sm:text-[3.5vh] font-bold mb-2 libre-baskerville-regular uppercase">
+            <div class="relative flex justify-center items-center h-[25vh] sm:h-[30vh] md:h-[40vh] xl:h-[35vh]  my-12 w-full md:w-[75%] text-white px-[20%]">
+              <img src="<?= esc_url($leftPathURL) ?>" alt="c-left" class="absolute top-0 left-0 h-full opacity-0">
+              <div class="flex flex-col justify-center w-full">
+                <h2 class="libre-baskerville-regular text-left text-[2.5vh] justify-evenly">
                   <?php _e('SZEROKA SIEĆ KONTAKTÓW', $text_domain) ?>
                 </h2>
-                <p class="text-left text-[3vw] sm:text-[3.5vw] inter-regular">
+                <p class="inter-regular text-left justify-evenly text-[1.5vh]">
                   <?php _e('Pomagamy w pozyskiwaniu klientów i nawiązywaniu wartościowych relacji biznesowych', $text_domain) ?>
                 </p>
               </div>
-              <img src="<?= esc_url($rightPathURL) ?>" alt="C-right" class="absolute right-0 top-0 z-10 h-full">
+              <img src="<?= esc_url($rightPathURL) ?>" alt="c-right" class="absolute top-0 right-0 h-full">
             </div>
 
             <!-- PILL 4 -->
-            <div class="relative my-12 sm:my-16 w-screen h-[40vh] sm:h-[45vh] rounded-full flex items-center">
-              <img src="<?= esc_url($leftPathURL) ?>" alt="C-left" class="absolute left-0 top-0 z-10 h-full">
-              <div class="text-white flex-1 px-[60px] sm:px-[80px]">
-                <h2 class="text-left tracking-[0.1em] text-[3vh] sm:text-[3.5vh] font-bold mb-2 libre-baskerville-regular uppercase">
+            <div class="relative flex justify-center items-center h-[25vh] sm:h-[30vh] md:h-[40vh] xl:h-[35vh]  my-12 w-full md:w-[75%] text-white px-[20%]">
+              <img src="<?= esc_url($leftPathURL) ?>" alt="c-left" class="absolute top-0 left-0 h-full">
+              <div class="flex flex-col justify-center w-full">
+                <h2 class="libre-baskerville-regular text-left text-[2.5vh] justify-evenly">
                   <?php _e('EKSPERCI OD SKANDYNAWII', $text_domain) ?>
                 </h2>
-                <p class="text-left text-[3vw] sm:text-[3.5vw] inter-regular">
+                <p class="inter-regular text-left justify-evenly text-[1.5vh]">
                   <?php _e('Znamy biznes, prawo i kulturę krajów nordyckich', $text_domain) ?>
                 </p>
               </div>
-              <img src="<?= esc_url($rightPathURL) ?>" alt="C-right" class="absolute right-0 top-0 z-10 h-full opacity-0">
+              <img src="<?= esc_url($rightPathURL) ?>" alt="c-right" class="absolute top-0 right-0 h-full opacity-0">
             </div>
-
-          </div>
-        </div>
       </div>
 
-      <div class="hidden md:block mb-24 md:ml-[8%] 2xl:ml-[15%] dlaczego-my">
+      <div class="block max-[1101px]:hidden mb-24 min-[1100px]:ml-[8%] 2xl:ml-[15%] dlaczego-my">
           <div class="scrollable-wrapper-unified">
               <div class="scrollable-content-unified w-[50vw] xl:w-[45vw] 2xl:w-[30vw]">
                   <!-- PILL DEKORACYJNY -->
@@ -170,9 +163,9 @@ $pill_image_3 = get_field('pill_image_3', $post_id);
                   </div>
                     
                   <!-- PILL 1 -->
-                  <div class="flex justify-between items-center h-[40vh] xl:h-[35vh]  my-12 w-full text-white">
-                    <img src="<?= esc_url($leftPathURL) ?>" alt="c-left" class="h-full opacity-0">
-                    <div class="flex flex-col justify-center max-w-[<?= $doubleWidth ?>px] w-full overflow-hidden">
+                  <div class="relative flex justify-center items-center h-[40vh] xl:h-[35vh]  my-12 w-full text-white px-[20%]
+                    <img src="<?= esc_url($leftPathURL) ?>" alt="c-left" class="absolute top-0 left-0 h-full opacity-0">
+                    <div class="flex flex-col justify-center w-full">
                       <h2 class="libre-baskerville-regular text-left text-[2.5vh] justify-evenly">
                         <?php _e('DOŚWIADCZENIE', $text_domain) ?>
                       </h2>
@@ -180,12 +173,12 @@ $pill_image_3 = get_field('pill_image_3', $post_id);
                         <?php _e('Lata praktyki w międzynarodowej administracji biznesowej', $text_domain) ?>
                       </p>
                     </div>
-                    <img src="<?= esc_url($rightPathURL) ?>" alt="c-right" class="h-full">
+                    <img src="<?= esc_url($rightPathURL) ?>" alt="c-right" class="absolute top-0 right-0 h-full">
                   </div>
-                  
+
                   <!-- PILL 2 -->
-                  <div class="flex justify-between item-center h-[40vh] xl:h-[35vh]  my-12 w-full text-white">
-                    <img src="<?= esc_url($leftPathURL) ?>" alt="c-left" class="h-full">
+                  <div class="relative flex justify-center items-center h-[40vh] xl:h-[35vh]  my-12 w-full text-white px-[20%]">
+                    <img src="<?= esc_url($leftPathURL) ?>" alt="c-left" class="absolute top-0 left-0 h-full">
                     <div class="flex flex-col justify-center w-full">
                       <h2 class="libre-baskerville-regular text-left text-[2.5vh] justify-evenly">
                         <?php _e('INDYWIDUALNE PODEJŚCIE', $text_domain) ?>
@@ -194,12 +187,12 @@ $pill_image_3 = get_field('pill_image_3', $post_id);
                         <?php _e('Strategia dopasowana do Twoich potrzeb', $text_domain) ?>
                       </p>
                     </div>
-                    <img src="<?= esc_url($rightPathURL) ?>" alt="c-right" class="h-full opacity-0">
+                    <img src="<?= esc_url($rightPathURL) ?>" alt="c-right" class="absolute top-0 right-0 h-full opacity-0">
                   </div>
-
+                  
                   <!-- PILL 3 -->
-                  <div class="flex justify-between item-center h-[40vh] xl:h-[35vh]  my-12 w-full text-white">
-                    <img src="<?= esc_url($leftPathURL) ?>" alt="c-left" class="h-full opacity-0">
+                  <div class="relative flex justify-center items-center h-[40vh] xl:h-[35vh]  my-12 w-full text-white px-[20%]">
+                    <img src="<?= esc_url($leftPathURL) ?>" alt="c-left" class="absolute top-0 left-0 h-full opacity-0">
                     <div class="flex flex-col justify-center w-full">
                       <h2 class="libre-baskerville-regular text-left text-[2.5vh] justify-evenly">
                         <?php _e('SZEROKA SIEĆ KONTAKTÓW', $text_domain) ?>
@@ -208,12 +201,12 @@ $pill_image_3 = get_field('pill_image_3', $post_id);
                         <?php _e('Pomagamy w pozyskiwaniu klientów i nawiązywaniu wartościowych relacji biznesowych', $text_domain) ?>
                       </p>
                     </div>
-                    <img src="<?= esc_url($rightPathURL) ?>" alt="c-right" class="h-full">
+                    <img src="<?= esc_url($rightPathURL) ?>" alt="c-right" class="absolute top-0 right-0 h-full">
                   </div>
 
                   <!-- PILL 4 -->
-                  <div class="flex justify-between item-center h-[40vh] xl:h-[35vh]  my-12 w-full text-white">
-                    <img src="<?= esc_url($leftPathURL) ?>" alt="c-left" class="h-full">
+                  <div class="relative flex justify-center items-center h-[40vh] xl:h-[35vh]  my-12 w-full text-white px-[20%]">
+                    <img src="<?= esc_url($leftPathURL) ?>" alt="c-left" class="absolute top-0 left-0 h-full">
                     <div class="flex flex-col justify-center w-full">
                       <h2 class="libre-baskerville-regular text-left text-[2.5vh] justify-evenly">
                         <?php _e('EKSPERCI OD SKANDYNAWII', $text_domain) ?>
@@ -222,7 +215,7 @@ $pill_image_3 = get_field('pill_image_3', $post_id);
                         <?php _e('Znamy biznes, prawo i kulturę krajów nordyckich', $text_domain) ?>
                       </p>
                     </div>
-                    <img src="<?= esc_url($rightPathURL) ?>" alt="c-right" class="h-full opacity-0">
+                    <img src="<?= esc_url($rightPathURL) ?>" alt="c-right" class="absolute top-0 right-0 h-full opacity-0">
                   </div>
               </div>
           </div>
